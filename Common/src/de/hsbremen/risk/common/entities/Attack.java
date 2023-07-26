0d61883d67963fb76b289ed6a68fa78d6673c1db
Nils <niehoelzz@gmail.com>
2023-07-26 19:32:57 +0200

    Added attack and attack result JavaDoc comments

--- Common/src/de/hsbremen/risk/common/entities/Attack.java
+++ Common/src/de/hsbremen/risk/common/entities/Attack.java
@@ -7,64 +7,119 @@ public class Attack implements Serializable {
     @Serial
     private static final long serialVersionUID = 8659647285194270403L;
 
-    private Player attackingPlayer;
+    private final Player attackingPlayer;
     private Player defendingPlayer;
     private int originCountry;
     private int targetCountry;
     private int amount;
-    private boolean isRematch;
 
+    /**
+     * Initializes a new attack.
+     *
+     * @param attackingPlayer Player object of the player attacking.
+     */
     public Attack(Player attackingPlayer) {
         this.attackingPlayer = attackingPlayer;
         this.originCountry = -1;
         this.targetCountry = -1;
         this.amount = -1;
-        this.isRematch = false;
     }
 
+    /**
+     * Resets the attack object to its initial state.
+     */
     public void reset() {
         this.originCountry = -1;
         this.targetCountry = -1;
         this.amount = -1;
-        this.isRematch = false;
     }
 
+    /**
+     * Checks if the attack has an origin country set.
+     *
+     * @return A Boolean value whether the attack has an origin country or not.
+     */
     public boolean hasOriginCountry() {
         return this.originCountry != -1;
     }
 
+    /**
+     * Retrieves the ID of the origin country of the attack.
+     *
+     * @return An Integer of the ID of the attacks origin country.
+     */
     public int getOriginCountry() {
         return originCountry;
     }
 
+    /**
+     * Sets the origin country of the attack.
+     *
+     * @param originCountry ID of the origin country that should be set for the attack.
+     */
     public void setOriginCountry(int originCountry) {
         this.originCountry = originCountry;
     }
 
+    /**
+     * Retrieves the ID of the target country of the attack.
+     *
+     * @return An Integer of the ID of the attacks target country.
+     */
     public int getTargetCountry() {
         return targetCountry;
     }
 
+    /**
+     * Sets the target country of the attack.
+     *
+     * @param targetCountry ID of the target country that should be set for the attack.
+     */
     public void setTargetCountry(int targetCountry) {
         this.targetCountry = targetCountry;
     }
 
+    /**
+     * Retrieves the amount of units used by the attacker for the attack.
+     *
+     * @return An Integer of the amount of units used for the attack.
+     */
     public int getAmount() {
         return amount;
     }
 
+    /**
+     * Sets the amount of units used by the attacker for the attack.
+     *
+     * @param amount Integer representing the amount of units that shall be used.
+     */
     public void setAmount(int amount) {
         this.amount = amount;
     }
 
+    /**
+     * Retrieves the player occupying the target country of the attack.
+     *
+     * @return Player object of the occupant of the attacks target country.
+     */
     public Player getDefendingPlayer() {
         return defendingPlayer;
     }
 
+    /**
+     * Sets the player defending the attack.
+     *
+     * @param defendingPlayer Player object of the defending player.
+     */
     public void setDefendingPlayer(Player defendingPlayer) {
         this.defendingPlayer = defendingPlayer;
     }
 
+    /**
+     * Retrieves the player occupying the origin country of the attack.
+     *
+     * @return Player object of the attacking player.
+     */
     public Player getAttackingPlayer() {
         return attackingPlayer;
     }
--- Common/src/de/hsbremen/risk/common/entities/AttackResult.java
+++ Common/src/de/hsbremen/risk/common/entities/AttackResult.java
@@ -10,13 +10,19 @@ public class AttackResult implements Serializable {
     @Serial
     private static final long serialVersionUID = 528324944731843347L;
 
-    private ArrayList<Integer> attackingRolls;
-    private ArrayList<Integer> defendingRolls;
+    private final ArrayList<Integer> attackingRolls;
+    private final ArrayList<Integer> defendingRolls;
     private int winningAttackingDice;
     private int winningDefendingDice;
     private boolean hasBeenResolved;
     private boolean attackerWon;
 
+    /**
+     * Initializes an AttackResult object.
+     *
+     * @param attackingRolls Amount of dice used by the attacker.
+     * @param defendingRolls Amount of dice used by the defendant.
+     */
     public AttackResult(int attackingRolls, int defendingRolls) {
         this.attackingRolls = new ArrayList<>();
         this.defendingRolls = new ArrayList<>();
@@ -37,6 +43,9 @@ public class AttackResult implements Serializable {
         this.evaluateDices();
     }
 
+    /**
+     * Evaluates the dice rolls and sets the result
+     */
     private void evaluateDices() {
         int minSize = Math.min(attackingRolls.size(), defendingRolls.size());
 
@@ -56,34 +65,75 @@ public class AttackResult implements Serializable {
         }
     }
 
+    /**
+     * Sets whether or not the attack has been resolved or has to continue.
+     *
+     * @param hasBeenResolved Boolean value if the fight has been resolved.
+     */
     public void setHasBeenResolved(boolean hasBeenResolved) {
         this.hasBeenResolved = hasBeenResolved;
     }
 
+    /**
+     * Retrieves if the attack has been resolved.
+     *
+     * @return A Boolean value representing whether or not the attack has been resolved.
+     */
     public boolean hasBeenResolved() {
         return hasBeenResolved;
     }
 
+    /**
+     * Retrieves the amount of winning dice of the attacker.
+     *
+     * @return An Integer of the amount of winning dice of the attacker.
+     */
     public int getWinningAttackingDice() {
         return winningAttackingDice;
     }
 
+    /**
+     * Retrieves the amount of winning dice of the defendant.
+     *
+     * @return An Integer of the amount of winning dice of the defendant.
+     */
     public int getWinningDefendingDice() {
         return winningDefendingDice;
     }
 
+    /**
+     * Retrieves the dice results of the attacking rolls.
+     *
+     * @return An ArrayList containing the Integers of the attackers dice results.
+     */
     public ArrayList<Integer> getAttackingRolls() {
         return attackingRolls;
     }
 
+
+    /**
+     * Retrieves the dice results of the defending rolls.
+     *
+     * @return An ArrayList containing the Integers of the defendants dice results.
+     */
     public ArrayList<Integer> getDefendingRolls() {
         return defendingRolls;
     }
 
+    /**
+     * Retrieves whether or not the attack was successful.
+     *
+     * @return A Boolean value representing whether or not the attack was successful.
+     */
     public boolean hasAttackerWon() {
         return attackerWon;
     }
 
+    /**
+     * Sets whether or not the attack was successful.
+     *
+     * @param attackerWon Boolean representing whether or not the attack was successful.
+     */
     public void setAttackerWon(boolean attackerWon) {
         this.attackerWon = attackerWon;
     }
